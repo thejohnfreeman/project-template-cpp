f65aea62eafd29d635c2cf1585247b4450012c7d
John Freeman <jfreeman08@gmail.com>
2021-09-05 18:39:49 -0500

    Why are install directories wrong on Windows?

--- 00-upstream/cmake/FutureInstallDirs.cmake
+++ 00-upstream/cmake/FutureInstallDirs.cmake
@@ -1,7 +1,20 @@
+if(FUTURE_INSTALL_DIRS)
+  return()
+endif()
+set(FUTURE_INSTALL_DIRS TRUE)
+message(STATUS "WIN32 = ${WIN32}")
 if(WIN32)
   # On Windows, everything just gets thrown into the same directory.
-  set(CMAKE_INSTALL_LIBDIR)
-  set(CMAKE_INSTALL_BINDIR)
+  set(CMAKE_INSTALL_INCLUDEDIR .)
+  set(CMAKE_INSTALL_LIBDIR .)
+  set(CMAKE_INSTALL_BINDIR .)
+  foreach(dir INCLUDEDIR LIBDIR BINDIR)
+    set(CMAKE_INSTALL_FULL_${dir}
+      ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_${dir}})
+  endforeach()
 else()
   include(GNUInstallDirs)
 endif()
+# Installed executables should be able to find installed shared libraries by
+# default.
+set(CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_FULL_LIBDIR})
