15018a5047120143cd569e049e126165a8ab5b0f
John Freeman <jfreeman08@gmail.com>
2022-03-04 17:31:10 -0600

    Extract cupcake_install_project

---
+++ 00-upstream/cmake/cupcake_install_project.cmake
@@ -0,0 +1,43 @@
+if(DEFINED_CUPCAKE_INSTALL_PROJECT)
+  return()
+endif()
+set(DEFINED_CUPCAKE_INSTALL_PROJECT TRUE)
+
+include(CMakePackageConfigHelpers)
+include(GNUInstallDirs)
+
+macro(cupcake_install_project)
+  set(CMAKE_INSTALL_EXPORTDIR "${CMAKE_INSTALL_LIBDIR}/cmake")
+  if(WIN32)
+    set(CMAKE_INSTALL_EXPORTDIR .)
+  endif()
+
+  set(CMAKE_CURRENT_EXPORT_DIR "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}")
+
+  export(EXPORT ${PROJECT_EXPORT_SET}
+    FILE "${CMAKE_CURRENT_EXPORT_DIR}/${PROJECT_NAME}-targets.cmake"
+    NAMESPACE ${PROJECT_NAME}::
+  )
+
+  configure_package_config_file("${CMAKE_CURRENT_SOURCE_DIR}/cmake/package-config.cmake.in"
+    "${CMAKE_CURRENT_EXPORT_DIR}/${PROJECT_NAME}-config.cmake"
+    INSTALL_DESTINATION "${CMAKE_INSTALL_EXPORTDIR}/${PROJECT_NAME}}"
+    NO_SET_AND_CHECK_MACRO
+    NO_CHECK_REQUIRED_COMPONENTS_MACRO
+  )
+
+  set(PROJECT_COMPATIBILITY SameMajorVersion)
+  if(${PROJECT_VERSION_MAJOR} EQUAL 0)
+    set(PROJECT_COMPATIBILITY SameMinorVersion)
+  endif()
+  write_basic_package_version_file(
+    "${CMAKE_CURRENT_EXPORT_DIR}/${PROJECT_NAME}-config-version.cmake"
+    VERSION ${PROJECT_VERSION}
+    COMPATIBILITY ${PROJECT_COMPATIBILITY}
+  )
+
+  install(
+    DIRECTORY "${CMAKE_CURRENT_EXPORT_DIR}"
+    DESTINATION "${CMAKE_INSTALL_EXPORTDIR}"
+  )
+endmacro()
