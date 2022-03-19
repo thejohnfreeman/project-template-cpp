e62441633959d9381225c5b53376d6d20da3462c
John Freeman <jfreeman08@gmail.com>
2022-03-19 17:30:23 -0500

    Change cupcake_find_package from function to macro

--- 00-upstream/cmake/cupcake_find_package.cmake
+++ 00-upstream/cmake/cupcake_find_package.cmake
@@ -6,18 +6,24 @@ set(DEFINED_CUPCAKE_FIND_PACKAGE TRUE)
 include(cupcake_add_dependency)
 
 # cupcake_find_package(<package-name> <version> [PRIVATE])
-function(cupcake_find_package name version)
-  message(STATUS "Finding package '${name}' depended by '${PROJECT_NAME}'...")
+# We cannot scope the call to find_package because we cannot predict which
+# variables that it sets need to percolate. Therefore, this must be a macro.
+macro(cupcake_find_package name version)
+  string(RANDOM cupcake_scope)
 
-  cmake_parse_arguments(ARG "PRIVATE" "" "" ${ARGN})
+  message(STATUS
+    "Finding package '${name}' depended by '${PROJECT_NAME}'..."
+  )
+
+  cmake_parse_arguments(${cupcake_scope} "PRIVATE" "" "" ${ARGN})
 
   # if(PROJECT_IS_TOP_LEVEL AND ...)
-  if(PROJECT_NAME STREQUAL CMAKE_PROJECT_NAME AND NOT ARG_PRIVATE)
+  if(PROJECT_NAME STREQUAL CMAKE_PROJECT_NAME AND NOT ${cupcake_scope}_PRIVATE)
     cupcake_add_dependency(${name} ${version})
   endif()
 
-  find_package(${name} ${version} REQUIRED ${ARG_UNPARSED_ARGUMENTS})
+  find_package(${name} ${version} REQUIRED ${${cupcake_scope}_UNPARSED_ARGUMENTS})
   if(NOT ${name}_FOUND)
     message(FATAL_ERROR "Package '${name}' was not found.")
   endif()
-endfunction()
+endmacro()
