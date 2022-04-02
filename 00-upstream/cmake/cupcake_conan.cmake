abb3fc0a6616757e23a55eaf6e8a27b0b1567901
John Freeman <jfreeman08@gmail.com>
2022-04-02 09:37:12 -0500

    Rename to install_cpp_info

--- 00-upstream/cmake/cupcake_conan.cmake
+++ 00-upstream/cmake/cupcake_conan.cmake
@@ -4,17 +4,17 @@ endif()
 set(DEFINED_CUPCAKE_CONAN TRUE)
 
 set(CUPCAKE_MODULE_DIR "${CMAKE_CURRENT_LIST_DIR}")
-file(READ "${CMAKE_CURRENT_LIST_DIR}/generate_cpp_info.cmake"
-  CUPCAKE_GENERATE_CPP_INFO
+file(READ "${CMAKE_CURRENT_LIST_DIR}/install_cpp_info.cmake"
+  CUPCAKE_INSTALL_CPP_INFO
 )
 
-function(cupcake_enable_conan)
+function(cupcake_install_cpp_info)
   install(
     CODE "
 string(TOUPPER $<CONFIG> CONFIG)
 set(CUPCAKE_MODULE_DIR \"${CUPCAKE_MODULE_DIR}\")
 set(PACKAGE_NAME ${PROJECT_NAME})
-${CUPCAKE_GENERATE_CPP_INFO}
+${CUPCAKE_INSTALL_CPP_INFO}
 "
     COMPONENT ${PROJECT_NAME}_development
   )
