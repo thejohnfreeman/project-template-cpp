bb3a7a5e904815c46e7181cb371532858d9a1b3a
John Freeman <jfreeman08@gmail.com>
2023-03-26 09:51:11 -0500

    Try to fix build of shared library on Windows

--- 11-no-cupcake/include/eleven/eleven.hpp
+++ 11-no-cupcake/include/eleven/eleven.hpp
@@ -1,9 +1,19 @@
 #ifndef ELEVEN_HPP
 #define ELEVEN_HPP
 
+#ifdef WIN32
+#  ifdef eleven_libeleven_EXPORTS
+     /* We are building this library. */
+#    define ELEVEN_EXPORT __declspec(dllexport)
+#  else
+     /* We are importing this library. */
+#    define ELEVEN_EXPORT __declspec(dllimport)
+#  endif
+#endif
+
 namespace eleven {
 
-unsigned int eleven();
+ELEVEN_EXPORT unsigned int eleven();
 
 }
 
