5d583a833d385c41c2e5aa57a1e57673ebb6d941
John Freeman <jfreeman08@gmail.com>
2023-04-03 14:39:37 -0500

    Alias long variable name

--- 08-find-module/external/Findzero.cmake
+++ 08-find-module/external/Findzero.cmake
@@ -1,74 +1,75 @@
-if(${CMAKE_FIND_PACKAGE_NAME}_FOUND)
-  message(STATUS "Package '${CMAKE_FIND_PACKAGE_NAME}' found previously.")
+set(package ${CMAKE_FIND_PACKAGE_NAME})
+if(${package}_FOUND)
+  message(STATUS "Package '${package}' found previously.")
   return()
 endif()
 
 # Set forwarding arguments.
-if(${CMAKE_FIND_PACKAGE_NAME}_FIND_VERSION_EXACT)
+if(${package}_FIND_VERSION_EXACT)
   set(_EXACT EXACT)
 else()
   unset(_EXACT)
 endif()
-if(${CMAKE_FIND_PACKAGE_NAME}_FIND_QUIETLY)
+if(${package}_FIND_QUIETLY)
   set(_QUIET QUIET)
 else()
   unset(_QUIET)
 endif()
 
 # Forwarding call to look for configuration file.
-find_package(${CMAKE_FIND_PACKAGE_NAME}
+find_package(${package}
   ${PACKAGE_FIND_VERSION}
   ${_EXACT}
   QUIET
   NO_MODULE
 )
 
-if(${CMAKE_FIND_PACKAGE_NAME}_FOUND)
-  message(STATUS "Package '${CMAKE_FIND_PACKAGE_NAME}' found by configuration file.")
+if(${package}_FOUND)
+  message(STATUS "Package '${package}' found by configuration file.")
   return()
 endif()
 
-find_path(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR
+find_path(${package}_INCLUDE_DIR
   NAMES zero
 )
-find_library(${CMAKE_FIND_PACKAGE_NAME}_LIBRARY
+find_library(${package}_LIBRARY
   NAMES zero
 )
-find_program(${CMAKE_FIND_PACKAGE_NAME}_EXECUTABLE
+find_program(${package}_EXECUTABLE
   NAMES true
 )
 
-# message(STATUS "${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR = ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR}")
-# message(STATUS "${CMAKE_FIND_PACKAGE_NAME}_LIBRARY = ${${CMAKE_FIND_PACKAGE_NAME}_LIBRARY}")
-# message(STATUS "${CMAKE_FIND_PACKAGE_NAME}_EXECUTABLE = ${${CMAKE_FIND_PACKAGE_NAME}_EXECUTABLE}")
+# message(STATUS "${package}_INCLUDE_DIR = ${${package}_INCLUDE_DIR}")
+# message(STATUS "${package}_LIBRARY = ${${package}_LIBRARY}")
+# message(STATUS "${package}_EXECUTABLE = ${${package}_EXECUTABLE}")
 
 # TODO: Write module to read version string from installed header.
 
 if(
-  ${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR AND
-  ${CMAKE_FIND_PACKAGE_NAME}_LIBRARY AND
-  ${CMAKE_FIND_PACKAGE_NAME}_EXECUTABLE
+  ${package}_INCLUDE_DIR AND
+  ${package}_LIBRARY AND
+  ${package}_EXECUTABLE
 )
   if(NOT TARGET zero::libzero)
     add_library(zero::libzero UNKNOWN IMPORTED)
     set_target_properties(zero::libzero PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR}"
-      IMPORTED_LOCATION "${${CMAKE_FIND_PACKAGE_NAME}_LIBRARY}"
-      IMPORTED_LOCATION_DEBUG "${${CMAKE_FIND_PACKAGE_NAME}_LIBRARY}"
-      IMPORTED_LOCATION_RELEASE "${${CMAKE_FIND_PACKAGE_NAME}_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${${package}_INCLUDE_DIR}"
+      IMPORTED_LOCATION "${${package}_LIBRARY}"
+      IMPORTED_LOCATION_DEBUG "${${package}_LIBRARY}"
+      IMPORTED_LOCATION_RELEASE "${${package}_LIBRARY}"
     )
 
     add_executable(zero::true IMPORTED)
     set_target_properties(zero::true PROPERTIES
-      IMPORTED_LOCATION "${${CMAKE_FIND_PACKAGE_NAME}_EXECUTABLE}"
-      IMPORTED_LOCATION_DEBUG "${${CMAKE_FIND_PACKAGE_NAME}_EXECUTABLE}"
-      IMPORTED_LOCATION_RELEASE "${${CMAKE_FIND_PACKAGE_NAME}_EXECUTABLE}"
+      IMPORTED_LOCATION "${${package}_EXECUTABLE}"
+      IMPORTED_LOCATION_DEBUG "${${package}_EXECUTABLE}"
+      IMPORTED_LOCATION_RELEASE "${${package}_EXECUTABLE}"
     )
   endif()
 
-  set(${CMAKE_FIND_PACKAGE_NAME}_FOUND TRUE)
-  message(STATUS "Package '${CMAKE_FIND_PACKAGE_NAME}' found installed.")
+  set(${package}_FOUND TRUE)
+  message(STATUS "Package '${package}' found installed.")
   return()
 endif()
 
-message(WARNING "Package '${CMAKE_FIND_PACKAGE_NAME}' not installed.")
+message(WARNING "Package '${package}' not installed.")
