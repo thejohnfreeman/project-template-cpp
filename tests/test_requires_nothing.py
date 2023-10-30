1e44f5badbc544fab828fe2cc6262d225e0afb0a
John Freeman <jfreeman08@gmail.com>
2023-10-29 21:18:22 -0500

    Add tests for six through eleven

---
+++ tests/test_requires_nothing.py
@@ -0,0 +1,10 @@
+from tests import helpers
+
+def test_seven(seven):
+    helpers.test(seven, '07-as-fc')
+
+def test_nine(nine):
+    helpers.test(nine, '09-external-project')
+
+def test_ten(ten):
+    helpers.test(ten, '10-conan')
---
+++ tests/test_requires_zero.py
@@ -0,0 +1,10 @@
+from tests import helpers
+
+def test_six(six):
+    helpers.test(six, '06-fp-fc')
+
+def test_eight(eight):
+    helpers.test(eight, '08-find-module')
+
+def test_eleven(eleven):
+    helpers.test(eleven, '11-no-cupcake')
